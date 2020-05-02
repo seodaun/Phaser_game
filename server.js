5a3d49c961ada5a7a02912dbd6df299226a99b94
seodaun <daun4799@naver.com>
2020-05-03 00:54:09 +0900

    modify data type

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 // server.js
 
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http); 
- 
+let express = require('express');
+let app = express();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
 app.use(express.static(__dirname + '/src/client/'));
 
 io.on('connection', function (socket) {
     console.log(`${socket.id} has joined!`);
     socket.on('disconnect', function () {
         console.log(socket.id + 'has leaved!');
-    });  
-    
+    });
+
 });
-var port = process.env.PORT || 8080;
+let port = process.env.PORT || 8080;
 http.listen(port, function () {
     console.log('server on!');
-    console.log('http://localhost:'+port);
+    console.log('http://localhost:' + port);
 }); 
